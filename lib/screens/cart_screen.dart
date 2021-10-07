1f131d8625e9913eac4ce8794d45e2ab1aea4737
Pawel Lak <pawellak@gmail.com>
2021-10-07 20:47:27 +0200

    Added: displaying a list of cart items.

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -15,28 +16,41 @@ class CartScreen extends StatelessWidget {
           title: const Text('Your cart'),
         ),
         body: Column(children: [
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Total', style: TextStyle(fontSize: 20)),
-                  const Spacer()  ,
-                  Chip(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    label: Text('\$${cart.totalAmount}'),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child:
-                        const Text('Order now', style: TextStyle(fontSize: 20)),
-                  )
-                ],
-              ),
-            ),
-            margin: const EdgeInsets.all(15),
-          )
+          summaryOfCart(context, cart),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+                itemCount: cart.items.length,
+                itemBuilder: (context, i) => CartItem(
+                    id: cart.items.values.toList()[i].id,
+                    price: cart.items.values.toList()[i].price,
+                    quantity: cart.items.values.toList()[i].quantity,
+                    title: cart.items.values.toList()[i].title)),
+          ),
         ]));
+  }
+
+  Card summaryOfCart(BuildContext context, Cart cart) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Total', style: TextStyle(fontSize: 20)),
+            const Spacer(),
+            Chip(
+              backgroundColor: Theme.of(context).primaryColor,
+              label: Text('\$${cart.totalAmount}'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Order now', style: TextStyle(fontSize: 20)),
+            )
+          ],
+        ),
+      ),
+      margin: const EdgeInsets.all(15),
+    );
   }
 }
