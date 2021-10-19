934697a924e30267707ef875166d5aee86e80de9
Pawel Lak <pawellak@gmail.com>
2021-10-19 20:14:45 +0200

    Refactoring: FutureBuilder instead of statefulWidget.

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,51 +4,45 @@ import 'package:shop_app/providers/orders.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
-  static String routeName = '/orders';
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
 
   const OrdersScreen({Key? key}) : super(key: key);
 
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-
-    super.initState();
-  }
-
+  //If order screen will be refreshing often, make stateful widget and in
+  //init state do:
+  //initState(){_myFuture = functionFuture ()}
+  //function will be like functionFuture(){return Provider.of<Orders>(context,listen:false).fetch}
+  //after that use _myFuture like a argument of future in futureBuilding.
   @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
     return Scaffold(
-      drawer: const AppDrawer(),
       appBar: AppBar(
-        title: const Text('Your orders'),
+        title: const Text('Your Orders'),
       ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(
-                order: ordersData.orders[i],
-              ),
-            ),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, _) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) =>
+                      OrderItem(order: orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
