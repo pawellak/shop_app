7c231978ea426ae68203fa17d60b751b6f739188
Pawel Lak <pawellak@gmail.com>
2021-10-24 21:16:58 +0200

    Modified: animation in auth screen handled by AnimatedContainer.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -202,17 +202,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, ch) => Container(
-            // height: _authMode == AuthMode.signup ? 320 : 260,
-            height: _heightAnimation.value.height,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+            curve: Curves.fastOutSlowIn,
+            height: _authMode == AuthMode.signup? 320:260,
             constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
+                BoxConstraints(minHeight: _authMode == AuthMode.signup? 320:260),
             width: deviceSize.width * 0.75,
             padding: const EdgeInsets.all(16.0),
-            child: ch),
-        child: Form(
+            child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             child: Column(
